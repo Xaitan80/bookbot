f49d6defaa9c39fdc88b1dac28c47936110d2d50
Xaitan <utrustad.sprang_0z@icloud.com>
2025-06-28 17:59:52 +0200

    now it works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, sort
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,8 +9,27 @@ def main():
     book_path = "books/frankenstein.txt"
     book_content = get_book_text(book_path)
     word_count = count_words(book_content)
+    c = count_characters(book_content)
+    sorted_list = sort(c)
 
-    print (f"{word_count} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frangenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for item_dict in sorted_list:
+       print(f"{item_dict['char']}: {item_dict['num']}")
+        
+
+
+
+    
+
+    #print (f"{word_count} words found in the document")
     #print (book_content)
+    #print (c)
+    #print (sorted_list)
+    
     
 main()
