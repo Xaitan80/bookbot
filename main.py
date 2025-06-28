5b31d9862f274d85984867e7cd611a3a204f9677
Xaitan <utrustad.sprang_0z@icloud.com>
2025-06-28 14:16:13 +0200

    looks nicer now

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import count_words
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
     
-def count_words(file_contents):
-    words = file_contents.split()
-    return len(words)
-
-
 def main():
     book_path = "books/frankenstein.txt"
     book_content = get_book_text(book_path)
