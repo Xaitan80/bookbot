e1367361b1d32a6e34b751e4ca069379ef9d534e
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-06 10:28:13 +0200

    progress

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from stats import *
+from stats import word_count
+from stats import char_count
+
 # Define a function to read the file content
 def get_book_text(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
-    
-'''def word_count(text):
-    words = text.split()
-    len(words)
-    return len(words)'''
-
 
 
 # Define a main function to use the above
@@ -18,6 +14,9 @@ def main():
     #print(book_text)
     words = word_count(book_text)
     print(f"{words} words found in the document")
+
+    characters = char_count(book_text)
+    print(characters)
 
 # Call the main function to run the program
 
--- stats.py
+++ stats.py
@@ -2,3 +2,17 @@ def word_count(text):
     words = text.split()
     len(words)
     return len(words)
+
+def char_count(text):
+    char_count = {}
+    for char in text:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] +=1
+        else:
+            char_count[char] = 1
+    return char_count
+
+def get_sorted_chars(char_dict):
+
+
